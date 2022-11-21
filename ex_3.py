9472c668fd58d45e339e7095da628fb044225c54
rahelehz <raheleh.zarandi@metropolia.fi>
2022-11-21 22:24:53 +0330

    exercise 9

--- ex_3.py
+++ ex_3.py
@@ -1,38 +1,34 @@
+# Again, extend the program by adding a new drive method that receives the number of hours as a parameter.
+# The method increases the travelled distance by how much the car has travelled in constant speed in the given time.
+# Example: The travelled distance of car object is 2000 km. The current speed is 60 km/h. Method call car.drive(1.5)
+# increases the travelled distance to 2090 km.
+
 class car:
-    def __init__(self,registration_number,maximum_speed,current_speed=0,travelled_distance=2000):
+    def __init__(self,registration_number, maximum_speed, current_speed,travelled_distance):
         self.registration_number=registration_number
         self.maximum_speed=maximum_speed
         self.current_speed=current_speed
         self.travelled_distance=travelled_distance
 
     def accerelate(self,change_speed):
-
-        #while current_speed > 0 and change_speed < 142:
-            if (change_speed < 0 ):
-
-                self.current_speed = self.current_speed + change_speed
-                print("speed is: ",self.current_speed)
-
-            else:
-                self.current_speed=self.current_speed + change_speed
-                print("speed is: ",self.current_speed)
+        if self.current_speed+change_speed<self.maximum_speed and self.current_speed+change_speed>0:
+            self.current_speed=change_speed +self.current_speed
+            print(f"the speed is {self.current_speed}")
 
 
-    def drive(self,hours):
-        self.travelled_distance = self.travelled_distance + self.current_speed * hours
-        print("Distance is: ",self.travelled_distance)
+        elif(self.current_speed+change_speed>self.maximum_speed):
+            print("current speed +50 is over maximum speed")
+        elif(self.current_speed + change_speed < 0):
+            print("current speed -200 is lower zero")
+
+    def distance(self,hours):
+        self.travelled_distance=self.travelled_distance+(self.current_speed*hours)
+        print(f"next distance is {machin.travelled_distance}")
 
 
 
-ford=car("ABC-123",142)
-ford.accerelate(60)
-ford.drive(1.5)
-ford.accerelate(70)
-ford.drive(1.5)
-ford.accerelate(50)
-ford.drive(1.5)
-ford.accerelate(-200)
-ford.drive(1.5)
 
 
-
+machin=car("ABC-123",142,60,2000)
+machin.distance(1.5)
+# print(f"next distance is {machin.travelled_distance}")
